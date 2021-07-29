01fa5b82649d80893d6571a312455dccbb3d3d4f
victorHSFerraz <victorf178@gmail.com>
2021-07-29 20:53:26 -0300

    service config

--- lib/components/constants.dart
+++ lib/components/constants.dart
@@ -1,3 +1,7 @@
 class Constants {
   static final String hasAccount = "hasAccount";
+
+  static final int connectTimeout = 25000;
+  static final int receiveTimeout = 15000;
+  static final String baseUrl = "https://swapi.dev/api/";
 }
