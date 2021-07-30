c1a60bcc591908b38ffef44e8276ec8e56533db2
victorHSFerraz <victorf178@gmail.com>
2021-07-29 21:41:48 -0300

    service dio

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -1,15 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:victor_flutter/components/constants.dart';
 
-class Service {}
-
-Dio dio = new Dio(
-  new BaseOptions(
-    baseUrl: Constants.baseUrl,
-    connectTimeout: Constants.connectTimeout,
-    receiveTimeout: Constants.receiveTimeout,
-    validateStatus: (status) {
-      return status <= 500;
-    },
-  ),
-);
+class Service {
+  Dio dio = new Dio(
+    new BaseOptions(
+      baseUrl: Constants.baseUrl,
+      connectTimeout: Constants.connectTimeout,
+      receiveTimeout: Constants.receiveTimeout,
+      validateStatus: (status) {
+        return status <= 500;
+      },
+    ),
+  );
+}
